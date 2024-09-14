014e843bfc99f60bf743cdfaf6648ff5e78d6a5d
asravna <145961269+asravna@users.noreply.github.com>
2024-09-14 18:06:52 +0200

    timeline

--- script.js
+++ script.js
@@ -6,15 +6,29 @@ function toggleMenu() {
 }
 
 
-function expandDetail(element) {
-    // Close any open details
-    var activeDetails = document.querySelectorAll(".detail.active");
-    activeDetails.forEach(function(detail) {
-        detail.classList.remove("active");
+// Function to expand the event box
+function expandBox(element) {
+    // Remove 'expanded' class from all other events
+    document.querySelectorAll('.timeline-event.expanded').forEach(event => {
+        event.classList.remove('expanded');
     });
-
-    // Toggle the current detail
-    var detail = element.querySelector(".detail");
-    detail.classList.toggle("active");
+    // Add 'expanded' class to the clicked element
+    element.classList.add('expanded');
 }
+
+
+
+function expandDetail(element) {
+    // Toggle the 'active' class on the clicked timeline-point
+    element.querySelector('.detail').classList.toggle('active');
+}
+
+function closeBox(event, element) {
+    // Prevent click from propagating to `timeline-point`
+    event.stopPropagation();
+    
+    // Remove 'active' class from the parent `detail` element
+    element.parentElement.classList.remove('active');
+}
+
 
